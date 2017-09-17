21415d87de9ca46558295742ee37548320358e38
ashivliving <ashivliving@gmail.com>
2017-09-17 21:06:22 +0530

    image adding capability added

--- client/js/script.js
+++ client/js/script.js
@@ -3,9 +3,7 @@ var apiUrl = 'http://localhost:3000/api/';
 var $title, $body, $image, $todoList;
 
 $(document).ready(function() {
-	$('#todoForm').on('submit', function(){
-		console.log('form submitted');
-	});
+	$('#todoForm').on('submit', formHandle);
 
 	$title = $('#title');
 	$body = $('#body');
@@ -24,12 +22,13 @@ function getTodos() {
 					 <div class="col-md-12">
 	                     <h1>`+todo.title+`</h1>
 	                     <div style="width:300px;">`;
+	                     console.log('image',todo.image);
 	                     if(todo.image){
 	                     	list += `<img src='/api/attachments/image/download/`+todo.image;
 	                     } else {
-	                     	list += `<img src='http://via.placeholder.com/350x150'`;
+	                     	list += `<img src='http://via.placeholder.com/350x150`;
 	                     }
-	                      list+= ` alt="post img" class="pull-left img-responsive thumb margin10 img-thumbnail"></div>
+	                      list+= `' alt="post img" class="pull-left img-responsive thumb margin10 img-thumbnail"></div>
 	                     <p style="padding:5px;" class="col-md-5 text-justify">
 	                         `+todo.body+`
 	                     </p>
@@ -41,4 +40,62 @@ function getTodos() {
 		});
 		$todoList.html(list);
 	});
+}
+
+function formHandle(e) {
+	e.preventDefault();
+	var todo = {
+		title: $title.val(),
+		body: $body.val()
+	}
+	console.log(todo);
+
+	    $.post(apiUrl + 'todos', todo).then(function(res) {
+
+        todo = res;
+
+        var promises = [];
+
+        if($image.val() != '') {
+            console.log('i need to process the Image upload');
+            promises.push(sendFile($image.get(0).files[0], apiUrl + 'attachments/image/upload'));
+        }
+
+        Promise.all(promises).then(function(results) {
+            if(promises.length >= 1) {
+                results.forEach(function(resultOb) {
+                    if(resultOb.result.files && resultOb.result.files.file[0].container) {
+                        todo[resultOb.result.files.file[0].container] = resultOb.result.files.file[0].name;
+                    }
+                });
+                console.dir(todo);
+                var id = todo.id;
+                delete todo.id;
+                $.post(apiUrl + 'todos/'+id+'/replace', todo).then(function() {
+                    getTodos();
+                });
+            } else {
+                getTodos();
+            }
+        });
+
+    });
+}
+
+function sendFile(file, url) {
+    return new Promise(function(resolve, reject) {
+
+        var xhr = new XMLHttpRequest();
+        var fd = new FormData();
+
+        xhr.open("POST", url, true);
+        xhr.onreadystatechange = function() {
+            if(xhr.readyState == 4 && xhr.status == 200) {
+                resolve(JSON.parse(xhr.responseText));
+            }
+        };
+        fd.append('file', file);
+        xhr.send(fd);
+
+    });
 }
